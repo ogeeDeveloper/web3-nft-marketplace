fe54aa71d379cb9e57382f7dd7e465786f8ce792
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-08 10:51:28 -0500

    Added the nessary packages and dependencies to the Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,11 @@
+import { useEffect, useState, useContext } from 'react'
+import { useRouter } from 'next/router'
+import { useTheme } from 'next-themes'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import images from '../assets'
+
 const Navbar = () => (
   <div>Navbar</div>
 )
